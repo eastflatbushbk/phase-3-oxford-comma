7229cda16cff2b9135e6ee03d57c951410c02fab
Robert Sainton <robertsainton@outlook.com>
2022-11-20 12:17:15 -0500

    completed

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,16 @@
 def oxford_comma(array)
+    if array.length < 3 
+         return array.join(" and ")
+        # puts array.join(" and ")
+    else
     last_string = array.pop
     last_string2 = "and #{last_string}"
     array2 = array.push(last_string2)
     
-    puts array2.join(", ")
-
+     return array2.join(", ")
+    # puts array2.join(", ")
+   end
 end
-oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])
+#  oxford_comma(["fiddleheads", "okra", "kohlrabi"])
+
+#   oxford_comma(["Tom", "Jerry"])
