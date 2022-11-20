85c8b900be0525f81b69357bdefc4086f53842c7
Robert Sainton <robertsainton@outlook.com>
2022-11-20 11:51:29 -0500

    completed

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
+    last_string = array.pop
+    last_string2 = "and #{last_string}"
+    array2 = array.push(last_string2)
+    
+    puts array2.join(", ")
 
 end
+oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])
